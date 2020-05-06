43777850c78efd0cc07d31d5909302ecfa0cd123
Andrew Chen <achen.this@gmail.com>
2020-05-05 21:06:55 -0400

    Changed to use Angular forms and pub/sub

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Post} from '../post.model';
+import {NgForm} from '@angular/forms';
+import {PostService} from '../post.service';
 
 @Component({
   selector: 'app-post-create',
@@ -8,7 +10,7 @@ import {Post} from '../post.model';
 })
 export class PostCreateComponent implements OnInit {
 
-  constructor() {
+  constructor(public postService: PostService) {
   }
 
   @Output() postCreated = new EventEmitter<Post>();
@@ -18,12 +20,12 @@ export class PostCreateComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddPost() {
-    const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
-    };
-    this.postCreated.emit(post);
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.postService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
 
 }
